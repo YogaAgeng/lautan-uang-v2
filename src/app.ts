f059eb65fa7b1194517412b93402d519a89e8a0a
ReppoIsYoga <yogafriend844@gmail.com>
2026-06-22 16:16:08 +0700

    feat: implement health check endpoint (Green State)

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-// Rute celah (sengaja dibiarkan kosong untuk TDD)
+// Implementasi rute untuk memuaskan tes
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: 'success' });
+});
 
 export default app;
